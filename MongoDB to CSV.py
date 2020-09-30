e7a215c985f1d7bb0195ec8a1b680e111af49150
abhishekgoyal-a11y <58354473+abhishekgoyal-a11y@users.noreply.github.com>
2020-09-30 15:37:28 +0530

    Update MongoDB to CSV.py

--- MongoDB to CSV.py
+++ MongoDB to CSV.py
@@ -5,10 +5,13 @@ from bson.json_util import dumps
 
 cluster = pymongo.MongoClient("mongodb://username:password@*************************************************/DatabaseName")
 
+# Enter DataBase Name
 mydb = cluster["DatabaseName"]
 
+# Enter Collection Name
 collection= mydb["CollectionName"]
 
+# Fill query or for all data leave empty
 document = collection.find({})
 
 json_string = dumps(document)
@@ -19,4 +22,5 @@ json_obj = json.loads(json_string)
 # Converting JSON Object to Pandas Dataframe
 data1 = json_normalize(json_obj)
 
+# Enter File Name
 data1.to_csv("FileName.csv")
